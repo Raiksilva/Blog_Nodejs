719ab7537d9ca2e0802abbe17e3302f4d1ba7d84
Raik silva <raiksilva@gmail.com>
2023-07-12 18:37:47 -0300

    Otimização dos processos centrais do projeto

--- src/index.js
+++ src/index.js
@@ -2,22 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection =require("./models/database");
+const Article = require("./models/Article");
+const Categoty = require("./models/Category");
+const categoriesController = require("./controller/categories/CategoriesController");
+const articlersController = require("./controller/articles/ArticlesController");
 
-//view engine
-app.set("view engine", "ejs");
-
-//public
-app.use(express.static('public'));
-
-//bodyParser
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json);
-
-
-//rotas
-app.get("/", (req, res) =>{
-    res.render("index")
-});
 
 //Database
 connection
@@ -27,6 +16,26 @@ connection
     }).catch((error) =>{
         console.log(error);
     });
+
+//view engine
+app.set("view engine", "ejs");
+
+//public
+app.use(express.static('public'));
+
+//bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use("/", categoriesController);
+
+app.use("/", articlersController);
+
+//rotas
+app.get("/", (req, res) =>{
+    res.render("index")
+});
 
 //server 
 app.listen(8181, function(erro) {
